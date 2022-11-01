2949f2b237bf2c6bf8d6051cca6fa599f786fd99
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-01 19:44:24 +0300

    sem1z2

---
+++ sem1z2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write ("Введите первое число: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите второе число: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+if (x1 > x2)
+   Console.WriteLine ("Первое - это max");
+if (x1 < x2)
+   Console.WriteLine ("Второе - это max");
+if (x1 == x2)
+Console.WriteLine ("Числа равны");
