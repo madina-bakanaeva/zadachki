a38082562a117a33442d3a6522fcf59347f71639
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-01 20:54:28 +0300

    sem1z6

---
+++ sem1z6/Program.cs
@@ -0,0 +1,8 @@
+﻿//Программа, которая на вход принимает число и выдает является ли оно четным (делится ли оно на два без остатка)
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+if (x%2 == 0)
+    Console.WriteLine("Число четное");
+else
+    Console.WriteLine("Число нечетное");
