f13cf92e2c6ea60d2ff715970b30ac31795a2518
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-01 20:13:10 +0300

    sem1z4

---
+++ sem1z4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая принимает на вход три числа и выдает максимальное из этихх чисел
+
+Console.Write ("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write ("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+max = a;
+if (b > a)
+   max = b;
+if (c > max)
+   max = c;
+Console.WriteLine (max);
