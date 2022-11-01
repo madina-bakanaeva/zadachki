2617ed6135642164547c0f2c4570bb513e575a2d
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-01 21:54:52 +0300

    sem1z8

---
+++ sem1z8/Program.cs
@@ -0,0 +1,9 @@
+﻿//Программа, которая на вход принимает число, а на выходе показывает все четные числа от 1 до N
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= n; i++)
+    if (i % 2==0)
+Console.Write(i + " ");
+
