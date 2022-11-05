7759b5f75e285263140c52f3ffe670f773079984
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-05 22:12:28 +0300

    sem2z15

---
+++ sem2z15/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает на вход цифру, обозначающую день недели и проверяет является ли этот день выходным
+
+
+
+Console.Clear();
+Console.WriteLine("Введите цифру недели:");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a == 6 || a == 7)
+    Console.WriteLine("Выходной.");
+if (a < 1 || a > 7)
+    Console.WriteLine("Нет такого дня.");
+if (a >= 1 && a < 5)
+    Console.WriteLine("Сегодня будний день.");
