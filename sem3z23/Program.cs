9e47b94b36c48fe26f722d94ace8c479814e2aa9
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-24 22:03:32 +0300

    sem3z23

---
+++ sem3z23/Program.cs
@@ -0,0 +1,9 @@
+﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i <= a; i++)
+{
+    Console.Write($"{i*i*i}  ");
+}
