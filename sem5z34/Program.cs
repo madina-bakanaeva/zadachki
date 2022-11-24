07b89b6c025174cd02c5d9927871b0f7c3b7bcf6
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-24 22:41:10 +0300

    sem5z34

---
+++ sem5z34/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(100, 1000);
+}
+
+int Kolichestvo(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+            count = count + 1;
+    }
+    return count;
+}
+
+Console.Clear();
+Console.WriteLine("Введите количество чисел в массиве: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+InputArray(array);
+Console.WriteLine($"[{string.Join(",", array)}]");
+Console.WriteLine($"Количество четных чисел: {Kolichestvo(array)}");
