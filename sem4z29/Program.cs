454c104827a92a8bcf13c6f9f939927571a46adf
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-24 22:33:24 +0300

    sem4z29

---
+++ sem4z29/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+Console.WriteLine("Введите количество элементов (8): ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[a];
+
+int i = 0;
+while (a < 8 || a > 8)
+{
+    Console.WriteLine("Необходим массив из 8 элементов! Введите:");
+    a = Convert.ToInt32(Console.ReadLine());
+}
+
+while (i < array.Length)
+{
+    Console.WriteLine("Введите " + i + " элемент массива :");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    i++;
+}
+Console.Write(" Массив: ");
+Console.WriteLine($"[{string.Join(",", array)}]");
