3a27be2d7eaf2ccec1e5ec5ca4b9d0a28adca13e
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-24 22:08:34 +0300

    sem4z25

---
+++ sem4z25/Program.cs
@@ -0,0 +1,15 @@
+﻿// Цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Clear();
+Console.WriteLine("Введите число А");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int result = 1;
+for (int i = 1; i <= b; i++)
+{
+    result = result * a;
+}
+Console.WriteLine(result);
+
+
