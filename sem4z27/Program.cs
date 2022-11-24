278d34650b5c7cf1cd45667f6edc1fc219d51cad
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-24 22:24:17 +0300

    sem4z27

---
+++ sem4z27/Program.cs
@@ -0,0 +1,15 @@
+﻿//Программа, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+Console.WriteLine("Введите  число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = 0;
+if (a < 0)
+    a = a * (-1);
+while (a > 0)
+{
+    int n = a % 10;
+    b = b + n;
+    a = a / 10;
+}
+Console.WriteLine(b);
