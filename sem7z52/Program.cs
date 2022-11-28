133b15d22ac90f11c41e8e9e4825c742854d629c
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-28 21:29:07 +0300

    sem7z52

---
+++ sem7z52/Program.cs
@@ -0,0 +1,50 @@
+﻿//52.  Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = new Random().Next(1, 10);
+    }
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+void AvgMatrix(int[,] matrix)
+{
+    double avg = 0;
+    double summa = 0;
+
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            summa += matrix[i, j];
+        }
+        avg = summa / matrix.GetLength(0);
+        Console.Write($"{avg} \t");
+        summa = 0;
+    }
+}
+
+Console.Clear();
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+Console.WriteLine("Начальный массив: ");
+PrintMatrix(matrix);
+Console.WriteLine("Среднее арифметическое столбцов: ");
+AvgMatrix(matrix);
