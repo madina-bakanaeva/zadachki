1248b245fed5bb7efee730815c72397ddba0bae0
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-29 20:56:53 +0300

    sem9z66

---
+++ sem9z66/Program.cs
@@ -0,0 +1,20 @@
+﻿
+//66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+
+int RecursiveSum(int a, int b)
+{
+    if (a == b)
+        return a;
+
+    return a + RecursiveSum(a + 1, b);
+}
+
+
+Console.Clear();
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(RecursiveSum(a, b));
+
