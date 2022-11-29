616e1d1b7be58af3dee90796aeeb198951d039c7
Маржанушка-ноут <madina.bakanaeva@mail.ru>
2022-11-29 20:13:42 +0300

    sem8z56

---
+++ sem8z56/Program.cs
@@ -0,0 +1,48 @@
+﻿//56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+
+void NewMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(-10, 10);
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void SotrRowInMatrix(int[,] matrix)
+{
+    int[] SumRow = new int[matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            SumRow[i] += matrix[i, j];
+        }
+    }
+    int temp = SumRow[0];
+    int num = 0;
+    for (int i = 0; i < SumRow.Length; i++)
+    {
+        if (SumRow[i] < temp)
+        {
+            temp = SumRow[i];
+            num = i + 1;
+        }
+    }
+    Console.WriteLine("Минимальная сумма элементов в строке:" + num);
+    Console.WriteLine("Минимальная сумма элементов равна:" + temp);
+}
+Console.Clear();
+Console.Write("Введите количество строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, m];
+Console.WriteLine("Начальный массив: ");
+NewMatrix(matrix);
+SotrRowInMatrix(matrix);
